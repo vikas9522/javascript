6030affb2e92f351d32f85b967a829b5d60db463
Vikas Tomar <105939651+vikas9522@users.noreply.github.com>
2024-06-17 15:52:13 +0530

    Create 02_Datatypes.js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3); 
+//code readability should be high
+console.log("hitesh");
+
+let name ="hitesh"
+let age = 18
+let isloggedin = false
+
+
+
+
+
+
+
+
+//numberm range 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null =>standalone value
+//undefined 
+//symbol => unique
+
+//object 
